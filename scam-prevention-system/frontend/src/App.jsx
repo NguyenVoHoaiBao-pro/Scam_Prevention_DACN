76c3112b97da044fc89a9c9688aeaf5f6ab6653c
thaibaoday <22130025@st.hcmuaf.edu.vn>
2026-04-07 14:12:13 +0700

    BE scanyourmessage(trainai va da quet duoc message)

--- scam-prevention-system/frontend/src/App.jsx
+++ scam-prevention-system/frontend/src/App.jsx
@@ -1,21 +1,29 @@
-//import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// Import các file giao diện của bạn
 import Login from "./pages/Login";
 import Report from "./pages/scamReport";
+import ScanContent from "./pages/ScanContent";
 
 function App() {
-  return <Report />;
- /* const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        // BẮT BUỘC PHẢI CÓ THẺ ROUTER NÀY BAO BỌC Ở NGOÀI CÙNG
+        <Router>
+            <Routes>
+                {/* Mặc định vào trang web sẽ nhảy sang /report */}
+                <Route path="/" element={<Navigate to="/report" replace />} />
 
-  return (
-    <>
-      {isLoggedIn ? (
-        <Report />
-      ) : (
-        <Login onLoginSuccess={() => setIsLoggedIn(true)} />
-      )}
-    </>
-  );*/
+                {/* Khai báo các đường dẫn */}
+                <Route path="/report" element={<Report />} />
+                <Route path="/scan" element={<ScanContent />} />
+                <Route path="/login" element={<Login />} />
 
+                {/* Bắt lỗi gõ link bậy */}
+                <Route path="*" element={<Navigate to="/report" replace />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
